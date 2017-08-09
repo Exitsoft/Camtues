79ccb5a5636f492344cff5bb0679a9393c839bc2
nayunhwan <nayunhwan.dev@gmail.com>
2017-08-09 16:07:23 +0900

    feat(About): add Yoonjeewoo profile

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET About page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'Express' });
+});
+
+/* GET About page. */
+router.get('/about/nayunhwan', function(req, res, next) {
+  res.render('nayunhwan', { title: 'Express' });
+});
+
 /* GET Login page. */
 router.get('/login', function(req, res, next) {
   res.render('login/login_page', { title: 'Express' });
